409cb9195e001711335d85ab5b6c458452b17e81
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2023-06-05 02:42:34 +0300

    Initial Commit

---
+++ SwiftCairo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftCairo
+//
+//  Created by Ahmed M. Hassan on 05/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
