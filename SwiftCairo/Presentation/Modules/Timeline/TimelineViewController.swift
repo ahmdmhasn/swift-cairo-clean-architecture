7c369c25790251becec96e4ed9cb2b65ecd302ce
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2023-06-07 02:37:05 +0300

    Enhance timeline view controller

--- SwiftCairo/Presentation/Modules/Timeline/TimelineViewController.swift
+++ SwiftCairo/Presentation/Modules/Timeline/TimelineViewController.swift
@@ -1,11 +1,18 @@
 import UIKit
+import Combine
 
 // MARK: TimelineViewController
 
 final class TimelineViewController: UIViewController {
+    // MARK: Outlets
+    private let tableView = UITableView()
+    
+    // MARK: Properties
     private let input: TimelineInput
     private let output: TimelineOutput
+    private var subscriptions: Set<AnyCancellable> = []
 
+    // MARK: Initialization
     init(viewModel: TimelineViewModelType) {
         self.input = viewModel.input
         self.output = viewModel.output
@@ -16,14 +23,45 @@ final class TimelineViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Set up UI and bindings
-        view.backgroundColor = .white
-        navigationItem.rightBarButtonItem = .init(systemItem: .add, primaryAction: UIAction { _ in
-            self.input.createTweetTrigger.send()
-        })
+        configureAppearance()
+        configureNavigationItem()
+        configureBinding()
         
         input.fetchTweetsTrigger.send()
+    }
+    
+    // MARK: Configuration
+    private func configureAppearance() {
+        view.backgroundColor = .white
+    }
+    
+    private func configureNavigationItem() {
+        let addTweetButtonItem = UIBarButtonItem(systemItem: .add, primaryAction: UIAction { [weak self] _ in
+            self?.input.createTweetTrigger.send()
+        })
+        
+        navigationItem.rightBarButtonItem = addTweetButtonItem
+    }
+    
+    private func configureBinding() {
+        output.$tweets
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: { [weak self] _ in self?.tableView.reloadData() })
+            .store(in: &subscriptions)
+    }
+}
+
+extension TimelineViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return output.tweets.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Reuse and configure the cell...
+        return UITableViewCell()
     }
 }
